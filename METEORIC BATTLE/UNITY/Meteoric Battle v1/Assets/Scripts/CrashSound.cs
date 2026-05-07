2130d69fad14bec4729e5afbd1e8bc5caa45be9a
CarlosBasallo <64237176+CarlosBasallo@users.noreply.github.com>
2026-05-07 03:23:20 -0500

    ADD sound and VFX

--- METEORIC BATTLE/UNITY/Meteoric Battle v1/Assets/Scripts/CrashSound.cs
+++ METEORIC BATTLE/UNITY/Meteoric Battle v1/Assets/Scripts/CrashSound.cs
@@ -5,6 +5,9 @@ public class CrashSound : MonoBehaviour
     public AudioClip crashClip;
     private AudioSource audioSource;
 
+    // --- TU PARTE: El hueco para poner las chispas ---
+    public GameObject efectoChispas;
+
     void Start()
     {
         audioSource = GetComponent<AudioSource>();
@@ -13,9 +16,17 @@ public class CrashSound : MonoBehaviour
 
     void OnCollisionEnter(Collision collision)
     {
+        // Esto es lo que ya tenían tus compañeros (El Sonido)
         if (crashClip != null && audioSource != null)
         {
             audioSource.PlayOneShot(crashClip);
+        }
+
+        // --- TU PARTE: Crear las chispas en el punto del choque ---
+        if (efectoChispas != null)
+        {
+            // collision.contacts[0].point es el lugar exacto donde se tocaron las naves
+            Instantiate(efectoChispas, collision.contacts[0].point + Vector3.back * 0.5f, Quaternion.identity);
         }
     }
 }
